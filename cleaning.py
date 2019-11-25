73075d1db978cbea6e8504609c9778b1655ec4e9
Carter Bouley <carterbouley@gmail.com>
2019-11-25 15:56:46 +0000

    further cleaning

--- cleaning.py
+++ cleaning.py
@@ -7,10 +7,11 @@ import json
 import isodate
 from apiclient.discovery import build
 class CleanYoutubeCSV():
+    
+    
     def __init__(self):
         pass
 
-        
         
     def add_dummies(self, df):
         df = df.drop(columns = 'Unnamed: 0')
@@ -32,11 +33,57 @@ class CleanYoutubeCSV():
         season_dummies = pd.get_dummies(df.season, prefix ='season')
         season_dummies.drop(columns = 'season_spring', inplace = True)
         
-        df2 = pd.concat([df, season_dummies], axis = 1)
+        df = pd.concat([df, season_dummies], axis = 1)
         
         day_part_dummies = pd.get_dummies(df.day_part, prefix = 'day_part')
         day_part_dummies.drop(columns = 'day_part_morning', inplace = True)
         
-        df3 = pd.concat([df2, day_part_dummies], axis = 1)
+        df = pd.concat([df, day_part_dummies], axis = 1)
         
-        return df3
+        indices = []
+        for item in list(enumerate(df.title.str.contains('Unboxing'))):
+            if item[1] == True:
+                indices.append(item[0])
+        for item in list(enumerate(df.title.str.contains('Review'))):
+            if item[1] == True:
+                indices.append(item[0])
+        for item in list(enumerate(df.title.str.contains('Impressions'))):
+            if item[1] == True:
+                indices.append(item[0])
+        indices = list(np.unique(indices))
+        for index in indices:
+            df.loc[index, 'review'] = 1
+        df.review.fillna(0, inplace = True)
+        df.review = df.review.astype('int')
+        
+        for item in list(enumerate(df.title.str.contains('Apple'))):
+            if item[1] == True:
+                indices.append(item[0])
+        indices = list(np.unique(indices))
+        for index in indices:
+            df.loc[index, 'apple'] = 1
+        df.apple.fillna(0, inplace = True)
+        df.apple = df.review.astype('int')
+        
+        for item in list(enumerate(df.title.str.contains('Google'))):
+            if item[1] == True:
+                indices.append(item[0])
+        indices = list(np.unique(indices))
+        for index in indices:
+            df.loc[index, 'google'] = 1
+        df.google.fillna(0, inplace = True)
+        df.google = df.review.astype('int')
+        
+        for item in list(enumerate(df.title.str.contains('Samsung'))):
+            if item[1] == True:
+                indices.append(item[0])
+        indices = list(np.unique(indices))
+        for index in indices:
+            df.loc[index, 'samsung'] = 1
+        df.samsung.fillna(0, inplace = True)
+        df.samsung = df.review.astype('int')
+        
+        return df
+    
+    
+    
